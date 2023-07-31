369ef5821ce8e46e26be50e55b15fb00c6903ba8
NitipoomKlaynium <57699052+NitipoomKlaynium@users.noreply.github.com>
2023-07-31 20:51:33 +0700

    Change

--- 1 Array & Hashing/1 Contain Duplicate (Easy)/Solution.py
+++ 1 Array & Hashing/1 Contain Duplicate (Easy)/Solution.py
@@ -1,12 +1,11 @@
 import re
 
-class Solution:
-    def containsDuplicate(nums: list[int]) -> bool:
-        nums.sort()
-        for i in range(len(nums) - 1) :
-            if (nums[i] == nums[i + 1]) :
-                return True
-        return False
+def containsDuplicate(nums: list[int]) -> bool:
+    nums.sort()
+    for i in range(len(nums) - 1) :
+        if (nums[i] == nums[i + 1]) :
+            return True
+    return False
 
 if __name__ == "__main__" :
-    print(Solution.containsDuplicate([1, 2, 3, 4]))
+    print(containsDuplicate([1, 2, 3, 4]))
--- 1 Array & Hashing/3 Two Sum/Solution.cpp
+++ 1 Array & Hashing/3 Two Sum/Solution.cpp
@@ -4,25 +4,22 @@
 
 using namespace std;
 
-class Solution {
-public:
-    static vector<int> twoSum(vector<int>& nums, int target) {
-        for (int first = 0 ; first < nums.size() - 1; first++) {
-            for (int second = first + 1 ; second < nums.size() ; second++) {
-                if (nums[first] + nums[second] == target) {
-                    vector<int> result = {first, second};
-                    return result;
-                }
+vector<int> twoSum(vector<int>& nums, int target) {
+    for (int first = 0 ; first < nums.size() - 1; first++) {
+        for (int second = first + 1 ; second < nums.size() ; second++) {
+            if (nums[first] + nums[second] == target) {
+                vector<int> result = {first, second};
+                return result;
             }
         }
-        vector<int> result = {-1, -1};
-        return result;
     }
-};
+    vector<int> result = {-1, -1};
+    return result;
+}
 
 int main() {
     vector<int> vec{2, 3, 4};
-    vector<int> result = Solution::twoSum(vec, 9);
+    vector<int> result = twoSum(vec, 9);
 
     for(int item: result) {
         cout << item << " ";
